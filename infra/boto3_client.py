b0668dfdd28c335a0116141c0d1e48ca323914ea
alynek <alyne0x67@gmail.com>
2025-03-09 18:16:16 -0300

    Add profile_name

--- infra/boto3_client.py
+++ infra/boto3_client.py
@@ -4,15 +4,18 @@ import os
 
 load_dotenv()
 bucket_name = os.getenv('S3_BUCKET_NAME')
+profile = os.getenv('PROFILE_NAME')
 
 
 def get_session():
-    return boto3.Session()
+    return boto3.Session(profile_name=profile)
 
 def get_bucket():
     return get_session().resource('s3').Bucket(bucket_name)
 
 def put_object(key, json_object, content_type):
+
+    print(bucket_name)
 
     get_bucket().put_object(
         Bucket=bucket_name, 
